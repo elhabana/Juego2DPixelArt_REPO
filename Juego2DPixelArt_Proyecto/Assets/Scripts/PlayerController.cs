0d0d1c2d892f11d5ab2114dd73bfecf98aaede15
elhabana <habanaparner@gmail.com>
2025-12-26 02:03:04 +0100

    Arreglitos de movilidad

--- Juego2DPixelArt_Proyecto/Assets/Scripts/PlayerController.cs
+++ Juego2DPixelArt_Proyecto/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Player Stats")]
-    [SerializeField] bool isFacingRight;
+    [Header("Player Configuration")]
+
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
 
     [Header("GroundCheck Configuration")]
     [SerializeField] bool isGrounded;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.2f;
+    [SerializeField] LayerMask groundLayer;
     
 
     [Header("Respawn Configuration")]
     [SerializeField] Transform respawnPoint;
 
     //Auto references
+    SpriteRenderer sr;
     Rigidbody2D rb;
     Animator anim;
     //General references
@@ -24,52 +30,85 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        Respawn();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        Flip();
+        AnimatorHandler();
     }
 
     private void FixedUpdate()
     {
-        
+        Movement();
+    
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Respawn();
+        }
     }
 
 
     void Respawn()
     {
-        
+        transform.position = respawnPoint.position;
     }
 
     void Movement()
     {
-        
+        rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
     }
+
+    
 
     void Flip()
     {
-        
+        if (moveInput.x > 0)
+        {
+            sr.flipX = false;
+        }
+        if (moveInput.x < 0)
+        {
+            sr.flipX = true;
+        }
     }
 
     void AnimatorHandler()
     {
-        
+        anim.SetBool("Jump", !isGrounded);
+        if (rb.linearVelocity.x != 0) anim.SetBool("Run", true);
+        else anim.SetBool("Run", false);
     }
 
     #region Input Methods
 
-    //Escribir Métodos de Input
+    //Escribir Mï¿½todos de Input
 
     #endregion
 
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
 
 }
--- Juego2DPixelArt_Proyecto/Assets/Scripts/PointsSystem.cs
+++ Juego2DPixelArt_Proyecto/Assets/Scripts/PointsSystem.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class PointsSystem : MonoBehaviour
 {
 
+    [Header("Points Management")]
+    [SerializeField] int actualPoints;
+    [SerializeField] int winPoints;
+    [SerializeField] GameObject winCoin;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        actualPoints = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (actualPoints <= winPoints) { winCoin.SetActive(true); }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (!collision.gameObject.CompareTag("PickUp"))
+        {
+            actualPoints += 1;
+            collision.gameObject.SetActive(false);
+        }
     }
 }
