60704259382cd4c911d4058eb2820b3a39f0f5a6
elhabana <habanaparner@gmail.com>
2026-01-02 09:34:04 +0100

    otra escena

--- Juego2DPixelArt_Proyecto/Assets/Scripts/PlayerController.cs
+++ Juego2DPixelArt_Proyecto/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public static PlayerController instance;
     public int vida = 3;
 
     [Header("Player Configuration")]
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour
     //General references
     Vector2 moveInput;
 
+    
     // Start is called before the first frame update
     void Start()
     {
