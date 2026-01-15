05e2fc906d69e6f9affade9cbe25f693c6ca9666
BREADfox27 <javierluna271107@gmail.com>
2026-01-15 20:14:01 +0100

    Boss y escenas Win y Lose
    El jefe ya tiene variable de vida y he duplicado la escena de EndGame para tener Win y Lose.

--- Juego2DPixelArt_Proyecto/Assets/Scripts/BossScript.cs
+++ Juego2DPixelArt_Proyecto/Assets/Scripts/BossScript.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossScript : MonoBehaviour
 {
+    public int life = 20;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,20 @@ public class BossScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (life <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == ("Bullet"))
+        {
+            life = life - 1;
+            Debug.Log("Ouch.");
+            Debug.Log(life);
+        }
     }
 }
